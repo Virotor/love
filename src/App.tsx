01691c17cf7c8a69b46657a9e090c5316da23c17
Kvolian <virotor.vik@gmail.com>
2024-02-11 21:15:44 +0300

    add icon

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Heart from './Heart/Heart'
+import { Menu } from './Menu/Menu';
+import { Address } from './Addres/Address';
 
 function App() {
   return (
     <div className="App">
       <Heart />
+      <Menu />
+      <Address />
     </div>
   );
 }
